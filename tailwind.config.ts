8b5aa9541078cbf53545b4546f7df077b7d8ed45
Kits <luck3801@gmail.com>
2024-07-11 20:15:17 -0300

    base page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        "enter": "0px 4px 0px 0px #33333329",
+        "footer": "0px -4px 4px 0px #00000029"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary": "rgb(254, 254, 254)",
+        "secondary": "rgb(222, 108, 92)",
+        "enter": "#0000001A"
+        
+      }
     },
   },
   plugins: [],
